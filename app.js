f58b581308227b88c7f5f0f0e0bd231aff85411d
User Name <user@email.com>
2025-11-13 18:21:43 -0700

    wordle plan

---
+++ app.js
@@ -0,0 +1,13 @@
+// pick random word = answer 
+//  SET number of guesses = 0
+// number of guesses =< 6
+// GET guess from player
+// IF guess is not 5 letters SHOW "guess must be 5 letters"
+// IF guess is not a real word SHOW "not a vaild word"
+// call result = checkGuess
+// IF letter is in the word in the right spot letter will turn Green
+// IF letter is in the word in the wrong spot letter will turn Purple
+// IF letter is not in the word letter will turn Gray
+// IF guess = answer show "You Win!" game stops
+// number of guesses= number of guesses +1
+// IF user uses all 6 guesses SHOW "Game Over, Answer was___" game stops
