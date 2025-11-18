71f56a35f466327b354b19a68724b946f8a2f921
User Name <user@email.com>
2025-11-18 13:56:04 -0700

    added js

--- app.js
+++ app.js
@@ -13,3 +13,37 @@
 // number of guesses= number of guesses +1
 // IF user uses all 6 guesses SHOW "Game Over, Answer was___" 
 // game stops
+
+/*-------------- Constants -------------*/
+const maxGuesses = 6;
+const wordLength = 5;
+const wordList = ['BLINK', 'BERRY', 'STRAY', 'DREAM', 'PEACH', 'INDIE', 'SCONE', 'TOPAZ', 'VINYL', 'SLOTH', 'STORM', 'HONEY', 'LIGHT'];
+const answer = wordList[Math.floor(Math.random() * wordList.length)];
+
+/*---------- Variables (state) ---------*/
+let currentGuess = '';
+let currentRow = 0;
+let currentCol = 0;
+let gameOver = false;
+
+/*----- Cached Element References  -----*/
+const squareEls = document.querySelectorAll(".sqr");
+const answerEl = document.getElementById("answer");
+/*-------------- Functions -------------*/
+
+
+/*----------- Event Listeners ----------*/
+document.addEventListener('keydown', function (e) {
+  if (gameOver) return;
+
+  const key = e.key.toUpperCase();
+
+  if (key >= 'A' && key <= 'Z' && key.length === 1) {
+    addLetter(key);
+  } else if (key === "BACKSPACE") {
+    deleteLetter();
+  } else if (key === "ENTER") {
+    submitGuess();
+  }
+
+
