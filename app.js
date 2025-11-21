6203e2c82fd68ce20e762686bb8628a0e77b06ae
User Name <user@email.com>
2025-11-20 17:25:01 -0700

    pushing

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// pick random word = answer 
-//  SET number of guesses = 0
-// number of guesses =< 6
-// GET guess from player
-// IF guess is not 5 letters SHOW "guess must be 5 letters"
-// IF guess is not a real word SHOW "not a vaild word"
-// call result = checkGuess
-// IF letter is in the word in the right spot letter will turn Green
-// IF letter is in the word in the wrong spot letter will turn Purple
-// IF letter is not in the word letter will turn Gray
-// IF guess = answer show "You Win!" 
-// game stops
-// number of guesses= number of guesses +1
-// IF user uses all 6 guesses SHOW "Game Over, Answer was___" 
-// game stops
 
 /*-------------- Constants -------------*/
 const maxGuesses = 6;
@@ -32,6 +17,10 @@ const squareEls = document.querySelectorAll(".sqr");
 const answerEl = document.getElementById("answer");
 const messageEl = document.getElementById("message");
 const resetBtn = document.getElementById("reset-btn");
+const instructionsBtn = document.getElementById("instructions-btn");
+const instructionsPopup = document.getElementById("instructions-popup");
+const closeInstructionsBtn = document.getElementById("close-instructions-btn");
+
 /*-------------- Functions -------------*/
 function addLetter(letter) {
   if (gameOver) return; 
@@ -146,3 +135,29 @@ if (gameOver) return;
 });
 
 resetBtn.addEventListener('click', resetGame);
+
+instructionsBtn.addEventListener('click', () => {
+  instructionsPopup.classList.remove('hidden');
+});
+
+closeInstructionsBtn.addEventListener('click', () => {
+  instructionsPopup.classList.add('hidden');
+});
+
+/*------------ Code Graveyard -------------*/
+
+// pick random word = answer 
+//  SET number of guesses = 0
+// number of guesses =< 6
+// GET guess from player
+// IF guess is not 5 letters SHOW "guess must be 5 letters"
+// IF guess is not a real word SHOW "not a vaild word"
+// call result = checkGuess
+// IF letter is in the word in the right spot letter will turn Green
+// IF letter is in the word in the wrong spot letter will turn Purple
+// IF letter is not in the word letter will turn Gray
+// IF guess = answer show "You Win!" 
+// game stops
+// number of guesses= number of guesses +1
+// IF user uses all 6 guesses SHOW "Game Over, Answer was___" 
+// game stops
